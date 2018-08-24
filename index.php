d298815170bb5cdd65f4e20f6c17f5f4966094cc
Andrew Quaye <andyquayegh@outlook.com>
2018-08-24 07:36:37 +0100

    create
    root file and folder

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew Quaye
+ * Date: 24/08/2018
+ * Time: 7:01 AM
+ */
+
+
+include_once "miner/function.module";
+
+if (isset($_REQUEST['coin'])){
+    if (isset($_GET['coin'])){
+        $coin = $_GET['coin'];
+    }else{
+        $coin = $_POST['coin'];
+    }
+
+    switch ($coin){
+
+        case "bitcoin";
+            BIT_COIN_CORE();
+        break;
+
+        case "bitcash";
+            bit_coin_core();
+        break;
+
+        case"dogecoin";
+            DOG_E_COIN();
+        break;
+
+        case"litecoin";
+            LITE_COIN();
+        break;
+
+        case"dash";
+            DASH();
+        break;
+
+        case"coinpot";
+            COIN_POT_TOKENS();
+        break;
+    }
+}
+?>
+
+<html>
+    <head>
+        <title>Coin</title>
+    </head>
+</html>
